8b2cb42b591b244dcc1010a0457019c78b12e07d
Oleksii Nikolskyi <oleksii.nikolskyi@gmail.com>
2023-11-13 17:58:39 +0200

    Implemented Strategy pattern

---
+++ EqualityAndComparisonStrategies/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace EqualityAndComparisonStrategies
+{
+    public class Person : IComparable<Person>, IComparable
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(ReferenceEquals(null, obj)) return 1;
+            if(ReferenceEquals(this, obj)) return 0;
+            return obj is Person other ? CompareTo(other) : throw new ArgumentException(nameof(obj));
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, this)) return 1;
+            return Id.CompareTo(other.Id);
+        }
+
+        private sealed class NameRelationalComparer
+            : IComparer<Person>
+        {
+            public int Compare(Person x, Person y)
+            {
+                if (ReferenceEquals(x, y)) return 0;
+                if(ReferenceEquals(null, x)) return -1;
+                if(ReferenceEquals(null, y)) return 1;
+                return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+            }
+        }
+
+        public static IComparer<Person> NameComparer { get; } 
+            = new NameRelationalComparer();
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            people.Sort(); // default sort strategy
+            //people.Sort((x, y) => x.Name.CompareTo(y.Name)); // strategy provided by lambda
+            people.Sort(Person.NameComparer); // sort by name
+        }
+    }
+}
